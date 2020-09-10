4488ff9bbfa9cc8a3cb8b66f82674e572960827c
Yuri <yuri.tayama@linksports.co.jp>
2020-09-11 06:56:46 +0900

    コメントの閲覧と作成リクエストのヘッダーにidTokenをつめる

--- src/store/index.ts
+++ src/store/index.ts
@@ -10,6 +10,9 @@ export default new Vuex.Store({
   state: {
     idToken: null,
   },
+  getters: {
+    idToken: (state) => state.idToken,
+  },
   mutations: {
     updateIdToken(state, idToken) {
       state.idToken = idToken;
---
+++ src/views/Comments.vue
@@ -0,0 +1,96 @@
+<template>
+  <div id="app">
+    <h3>掲示板に投稿する</h3>
+    <label for="name">ニックネーム：</label>
+    <input id="name" type="text" v-model="name" />
+
+    <br />
+    <br />
+
+    <label for="comment">コメント：</label>
+    <textarea id="comment" v-model="comment"></textarea>
+
+    <br />
+    <br />
+
+    <button @click="createComment">送信</button>
+
+    <h2>掲示板</h2>
+    <div v-for="post in posts" :key="post.name">
+      <br>
+      <div>名前：{{ post.fields.name.stringValue }}</div>
+      <div>コメント：{{ post.fields.comment.stringValue }}</div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import '../extensions';
+
+export default {
+  data() {
+    return {
+      name: '',
+      comment: '',
+      posts: [],
+    };
+  },
+  computed: {
+    idToken() {
+      return this.$store.getters.idToken;
+    },
+  },
+  created() {
+    axios.get(
+      '/comments', {
+        headers: { Authorization: `Bearer ${this.idToken}` },
+      },
+    ).then((response) => {
+      this.posts = response.data.documents;
+      console.log(response);
+    });
+  },
+  methods: {
+    createComment() {
+      if (this.name.isBlank() || this.comment.isBlank()) {
+        console.log('Toast: Blank!!');
+        return;
+      }
+
+      axios
+        .post(
+          '/comments',
+          {
+            fields: {
+              name: { stringValue: this.name },
+              comment: { stringValue: this.comment },
+            },
+          }, {
+            headers: { Authorization: `Bearer ${this.idToken}` },
+          },
+        )
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+
+      this.name = '';
+      this.comment = '';
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
