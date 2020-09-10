0dad4a57ae1ae27cde95e29e025fad687eb22d6a
Yuri <yuri.tayama@linksports.co.jp>
2020-09-10 23:36:47 +0900

    ルーターを実装

--- src/App.vue
+++ src/App.vue
@@ -1,87 +1,24 @@
 <template>
   <div id="app">
-    <h3>掲示板に投稿する</h3>
-    <label for="name">ニックネーム：</label>
-    <input id="name" type="text" v-model="name" />
-
-    <br />
-    <br />
-
-    <label for="comment">コメント：</label>
-    <textarea id="comment" v-model="comment"></textarea>
-
-    <br />
-    <br />
-
-    <button @click="createComment">送信</button>
-
-    <h2>掲示板</h2>
-    <div v-for="post in posts" :key="post.name">
-      <br>
-      <div>名前：{{ post.fields.name.stringValue }}</div>
-      <div>コメント：{{ post.fields.comment.stringValue }}</div>
-    </div>
-
+    <header>
+      <router-link to="/" class="header-item">掲示板</router-link>
+      <router-link to="/login" class="header-item">ログイン</router-link>
+      <router-link to="/register" class="header-item">登録</router-link>
+    </header>
+    <router-view></router-view>
   </div>
 </template>
 
-<script>
-import axios from 'axios';
-import './extensions';
-
-export default {
-  data() {
-    return {
-      name: '',
-      comment: '',
-      posts: [],
-    };
-  },
-  created() {
-    axios.get(
-      '/comments',
-    ).then((response) => {
-      this.posts = response.data.documents;
-      console.log(response);
-    });
-  },
-  methods: {
-    createComment() {
-      if (this.name.isBlank() || this.comment.isBlank()) {
-        console.log('Toast: Blank!!');
-        return;
-      }
-
-      axios
-        .post(
-          '/comments',
-          {
-            fields: {
-              name: { stringValue: this.name },
-              comment: { stringValue: this.comment },
-            },
-          },
-        )
-        .then((response) => {
-          console.log(response);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-
-      this.name = '';
-      this.comment = '';
-    },
-  },
-};
-</script>
-
-<style>
+// スコープ付きCSS: スタイルが現在のコンポーネントの要素にのみ適用される(=ローカルスタイルにできる)
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+.header-item {
+  padding: 10px;
 }
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,26 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
-import Home from '../views/Home.vue';
+import Comments from '../views/Comments.vue';
+import Login from '../views/Login.vue';
+import Register from '../views/Register.vue';
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home,
+    name: 'Comments',
+    component: Comments,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/login',
+    name: 'Login',
+    component: Login,
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register,
   },
 ];
 
